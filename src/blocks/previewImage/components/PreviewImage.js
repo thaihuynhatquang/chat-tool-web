861172bb60170e3c01c961a4e99e6a3cd9bc9546
Nhat Quang <thaihuynhatquang@gmail.com>
2020-06-01 23:47:06 +0700

    Web preview image

---
+++ src/blocks/previewImage/components/PreviewImage.js
@@ -0,0 +1,88 @@
+import React, { Fragment } from 'react';
+import Lightbox from 'react-images';
+import { compose, withStateHandlers, withProps } from 'recompose';
+
+const PreviewImage = compose(
+  withProps((props) => {
+    const { images, src } = props;
+    let thumbnails = [];
+
+    let indexImage = 0;
+    if (images) {
+      const indexMatch = images.findIndex((image) => image.src === src);
+      if (indexMatch >= 0) {
+        thumbnails = [...images];
+        indexImage = indexMatch;
+      } else {
+        thumbnails = [{ src }, ...images];
+      }
+    } else {
+      thumbnails = [{ src }];
+    }
+    return {
+      thumbnails,
+      indexImage,
+    };
+  }),
+  withStateHandlers(
+    (props) => ({
+      lightboxIsOpen: false,
+      currentImage: 0,
+    }),
+    {
+      openLightbox: ({ lightboxIsOpen, currentImage }) => (index) => ({
+        currentImage: index,
+        lightboxIsOpen: true,
+      }),
+      closeLightbox: ({ lightboxIsOpen, currentImage }) => () => ({
+        currentImage: 0,
+        lightboxIsOpen: false,
+      }),
+      gotoPrevious: ({ lightboxIsOpen, currentImage }) => () => ({
+        currentImage: currentImage - 1,
+        lightboxIsOpen,
+      }),
+      gotoNext: ({ lightboxIsOpen, currentImage }) => () => ({
+        currentImage: currentImage + 1,
+        lightboxIsOpen,
+      }),
+      gotoImage: ({ lightboxIsOpen }) => (index) => ({
+        currentImage: index,
+        lightboxIsOpen,
+      }),
+    },
+  ),
+);
+
+export default PreviewImage((newprops) => {
+  const {
+    lightboxIsOpen,
+    currentImage,
+    gotoNext,
+    gotoPrevious,
+    gotoImage,
+    closeLightbox,
+    openLightbox,
+    thumbnails,
+    src,
+    indexImage,
+    ...rest
+  } = newprops;
+
+  return (
+    <Fragment>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <img src={src} onClick={() => openLightbox(indexImage)} {...rest} />
+      <Lightbox
+        currentImage={currentImage}
+        images={thumbnails}
+        isOpen={lightboxIsOpen}
+        onClickNext={gotoNext}
+        onClickPrev={gotoPrevious}
+        onClickThumbnail={gotoImage}
+        onClose={closeLightbox}
+        showThumbnails={thumbnails.length > 1}
+      />
+    </Fragment>
+  );
+});
