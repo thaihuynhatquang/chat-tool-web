25deb4c969c604c3f53e48c6a30ebd9724d34ee3
Nhat Quang <thaihuynhatquang@gmail.com>
2020-06-24 13:06:08 +0700

    Fix endpoint

--- public/config/env-local.js
+++ public/config/env-local.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
 
 var CLIENT_ID = '745337904100-80mto7vit3mnp8nqhai5kbbuf2oa1d2l.apps.googleusercontent.com';
-var REACT_APP_API_URL = 'https://api/chatible.me';
+var REACT_APP_API_URL = 'https://api.chatible.me';
 var REACT_APP_URL = 'https://chatible.me';
